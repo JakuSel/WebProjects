8beb0c5c0dd6039ad869bd712749595fafa5091c
Jacob <jakub.seliga@akademiasovy.sk>
2018-01-24 21:05:41 +0100

    debuggin console.log

--- WebWeatherApi/js/script.js
+++ WebWeatherApi/js/script.js
@@ -1,30 +1,37 @@
-$(function(){
+(function(){
         
 $("#getBtn").on("click",function(){
     var city=$("#city").val();
     var code=$("#country_code").val();
-    var urllink="http://api.openweathermap.org/data/2.5/weather?q=";
+     
 if(city.lenght>1){
+var urllink= 'http://api.openweathermap.org/data/2.5/weather?q=';   
+urllink=urllink + city;
     
-urllink=urllink+city;
-    if(code.length == 2)
-        urllink=urllink + ',' + code;
-urllink=urllink +'&appid=6665fa2a88064f14b434d1879fb5f327'
-       console.log(urllink);
-    
-$.ajax({
-    url: urllink,
-    data:{format: "json"},
-    error: function(){
-         //chybova hlaška
-           },
-    dataType:"json",
-    success: function(data){
-				console.log(data.main.temp-273.15+"°C");
-				console.log(data.weather[0].description);
-           },
-    type:"GET"
-       });
+if(code.length == 2){
+    urllink=urllink + ',' + code;
 }
-   });
+        
+urllink=urllink + '&appid=6665fa2a88064f14b434d1879fb5f327';
+    console.log("Hello world");
+       console.log(urllink);
+ /*   
+$.ajax({
+    url: urlLink,
+    data: {format: 'json'},
+    error: function(){
+				//vypis chyby
+			},
+    dataType: "json",
+    success: function(data){
+console.log(data.main.temp-273.15+"°C");
+console.log(data.weather[0].description);
+			},
+    type: "GET"
+			});
+            */
+		}
+        
+	});
+    
 });
