03e526bcced0b6586a28790707fe76ba8924e409
Jakub <jakub.seliga@akademiasovy.sk>
2018-01-24 12:35:48 +0100

    project weather add ajax

--- WebWeatherApi/js/script.js
+++ WebWeatherApi/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     
+    
+    
    $("#getBtn").on("click",function(){
        var city=$("#city").val();
-       var code=$("#country_code").val();
+        var code=$("#country_code").val();
        var urllink='api.openweathermap.org/data/2.5/weather?q=';
        if(city.length>1){
            urllink=urllink+city;
@@ -11,6 +13,19 @@ $(document).ready(function(){
            urllink=urllink+','+code;
        
        urllink=urllink+'&appid=6665fa2a88064f14b434d1879fb5f327';
-       
+       console.log(urllink);
+       $.ajax({
+            url: urllink,
+            data:{format: 'json'},
+            error: function(){
+               //chybova hlaška
+           },
+           dataType:'json',
+           success: function(data){
+               console.log("temp:"+data.main.temp);
+               console.log("desc:"+data.weather[0].description);
+           },
+           type:'GET'
+       });
    });
 });
